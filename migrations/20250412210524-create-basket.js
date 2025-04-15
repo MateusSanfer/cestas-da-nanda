a0675a00299cc8dee299bca2335a4ebe3652d3da
Mateus Sanfer <mateus.sanfer@gmail.com>
2025-04-15 10:40:00 -0300

    chore: configura Sequelize e conexão com banco de dados MySQL

---
+++ migrations/20250412210524-create-basket.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Baskets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      price: {
+        type: Sequelize.FLOAT
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      images: {
+        type: Sequelize.JSON
+      },
+      includedItems: {
+        type: Sequelize.JSON
+      },
+      availableExtras: {
+        type: Sequelize.JSON
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Baskets');
+  }
+};
