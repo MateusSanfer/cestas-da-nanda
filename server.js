5ff03e2a72ce33c76482ffeec1571c0618882452
Mateus Sanfer <mateus.sanfer@gmail.com>
2026-01-08 22:34:03 -0300

    Remove rota de emergencia de admin

--- server.js
+++ server.js
@@ -20,27 +20,6 @@ app.use("/auth", authRoutes);
 const buildPath = path.join(__dirname, "build");
 app.use(express.static(buildPath));
 
-// Rota de Emergência para Admin
-app.get("/admin-fix-secret", async (req, res) => {
-  const email = req.query.email;
-  if (!email)
-    return res.send("Por favor, informe o email na URL: ?email=seu@email.com");
-
-  try {
-    const { User } = require("./models");
-    const user = await User.findOne({ where: { email } });
-
-    if (!user) return res.send(`Usuário ${email} não encontrado.`);
-
-    await user.update({ isAdmin: true });
-    res.send(
-      `SUCESSO! Usuário ${user.name} (${user.email}) agora é ADMIN. Tente fazer login.`
-    );
-  } catch (error) {
-    res.status(500).send("Erro: " + error.message);
-  }
-});
-
 // Redirecionar qualquer outra rota para o React
 app.get("*", (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
